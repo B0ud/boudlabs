8fe302be663598c707b62336aed77575f1e7e177
B0ud <mehdi.boudart@gmail.com>
2026-01-27 14:25:47 +0100

    RE assert

--- infrastructure/provisioning/main.tf
+++ infrastructure/provisioning/main.tf
@@ -60,22 +60,22 @@ output "worker_ips" {
   value = module.talos_cluster.worker_ips
 }
 
-#check "cluster_health" {
-#  data "http" "kube_api_health" {
-#    url      = "https://192.168.50.100:6443/livez"
-#    insecure = true
-#    retry {
-#      attempts     = 10
-#      min_delay_ms = 1000
-#      max_delay_ms = 5000
-#    }
-#  }
-#
-#  assert {
-#    # 200 = OK (Anonymous Auth activé)
-#    # 401 = Unauthorized (API UP mais Auth requise, ce qui est normal pour Talos sécurisé)
-#    condition     = contains([200, 401, 403], data.http.kube_api_health.status_code)
-#    error_message = "L'API Kubernetes n'est pas joignable (Status différent de 200/401/403). VIP: 192.168.50.100"
-#  }
-#}
-#
+check "cluster_health" {
+  data "http" "kube_api_health" {
+    url      = "https://192.168.50.100:6443/livez"
+    insecure = true
+    retry {
+      attempts     = 10
+      min_delay_ms = 1000
+      max_delay_ms = 5000
+    }
+  }
+
+  assert {
+    # 200 = OK (Anonymous Auth activé)
+    # 401 = Unauthorized (API UP mais Auth requise, ce qui est normal pour Talos sécurisé)
+    condition     = contains([200, 401, 403], data.http.kube_api_health.status_code)
+    error_message = "L'API Kubernetes n'est pas joignable (Status différent de 200/401/403). VIP: 192.168.50.100"
+  }
+}
+
